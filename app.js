f5ae0c6315ec020dac094f2a5c252d8ececd2d52
yalab <rudeboyjet@gmail.com>
2024-01-29 18:46:18 +0900

    Add socketMode, appToken, and port to app initialization

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ const { App } = require('@slack/bolt');
 // ボットトークンと Signing Secret を使ってアプリを初期化します
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: process.env.PORT || 3000
+});
+
+app.message(async ({ message, say }) => {
+  // イベントがトリガーされたチャンネルに say() でメッセージを送信します
+    console.log(message)
+    console.log(say)
+  await say(`hello`);
 });
 
 (async () => {
   // アプリを起動します
-  await app.start(process.env.PORT || 3000);
+  await app.start();
 
   console.log('⚡️ Bolt app is running!');
 })();
