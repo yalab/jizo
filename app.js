6a3625eacb995b3658c4da5afa28dbe6564eb409
yalab <rudeboyjet@gmail.com>
2025-05-05 18:30:16 +0900

    I've made it possible to set the model with environment variables.

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const dyingMessage = 'Help me. I have died....'
 const postOpenAI = async (messages) => {
   const chatCompletion = await openai.chat.completions.create({
       messages: messages,
-      model: 'gpt-4o',
+      model: process.env.OPENAI_MODEL || 'gpt-4o',
   })
   const message = chatCompletion.choices[0].message
   return message.content
